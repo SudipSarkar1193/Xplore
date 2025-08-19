5b108c011b6a40a6b123fffb36d45b2e028be37c
SudipSarkar1193 <official.sudipsarkar1193@gmail.com>
2025-08-19 22:47:23 +0530

    small change

--- src/components/common/CreatePostModal.jsx
+++ src/components/common/CreatePostModal.jsx
@@ -8,16 +8,16 @@ const CreatePostModal = () => {
 	const { isModalOpen, closeModal } = useModal();
 	const [scrollPosition, setScrollPosition] = useState(0);
 
-	const handleScroll = (e) => {
-		e.stopPropagation();
-	};
+	// const handleScroll = (e) => {
+	// 	e.stopPropagation();
+	// };
 
 	if (!isModalOpen) return null;
 
 	return (
 		<div
 			className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80"
-			onScroll={handleScroll}
+			// onScroll={handleScroll}
 		>
 			<div className="bg-black border-2 border-gray-700 rounded-2xl w-full max-w-2xl mx-4  relative">
 				<button
